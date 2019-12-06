32dda4d4895447ad21958fa97f91fee0265f7aec
emm7494 <emm7494@gmail.com>
2019-12-06 03:13:51 +0000

    Modified route matching.

--- public/index.php
+++ public/index.php
@@ -4,12 +4,13 @@ require_once '../controller/controllers.php';
 
 $uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
 
-if ('/index' === $uri) {
+if (in_array($uri, array('/', '/index', '/home'))) {
     render_home();
 } elseif ('/about' === $uri) {
     render_about();
 } elseif ('/contact' === $uri) {
     render_contact();
 } else {
+    header('HTTP/1.1 404 Not Found');
     render_404();
 }
